c426fc6a73df294f7552544d03fe871b7d612bf7
thinggggggggg <thomasgotch0413@hotmail.com>
2024-10-28 14:40:59 +0000

    flask stuff

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,14 @@ from app import app
 @app.route('/home')
 def home():
     user = {'username': 'willy'}
-    return render_template('home.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('home.html', title='Home', user=user, posts=posts)
