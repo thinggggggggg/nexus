e82ec7ba2413911ca0a74dcc4d1cd896cfe9ee3d
thinggggggggg <thomasgotch0413@hotmail.com>
2024-10-28 13:02:58 +0000

    smarter ui

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template
 from app import app
 
 @app.route('/')
-@app.route('/index')
-def index():
+@app.route('/home')
+def home():
     user = {'username': 'willy'}
-    return render_template('index.html', title='Home', user=user)
+    return render_template('home.html', title='Home', user=user)
