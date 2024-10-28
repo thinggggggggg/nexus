f28ad7a0b30e995a4206c186612de259a6af3993
thinggggggggg <thomasgotch0413@hotmail.com>
2024-10-28 18:01:27 +0000

    poo

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config)
+app = Flask(__name__)  # init flask object
+app.config.from_object(Config)  # enabling command to run to be just 'flask run'
 
-from app import routes
+from app import routes  # leave as last line !!!!!!!!!
