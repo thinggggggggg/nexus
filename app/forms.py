669443382294dfdb25580aee845dc8fc002f8703
thinggggggggg <thomasgotch0413@hotmail.com>
2024-10-29 14:44:41 +0000

    figured out flask

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):  # might change... css is better. looks nicer
-    username = StringField(validators=[DataRequired()])
-    password = PasswordField(validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    username = StringField(validators=[DataRequired()], id="input_field")
+    password = PasswordField(validators=[DataRequired()], id="input_field")
+    remember_me = BooleanField('Remember Me', id="boolean_field")
+    submit = SubmitField('Sign In', id="submit_field")
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -21,23 +21,30 @@ def home():
     messages = MESSAGES
     return render_template('home.html', title='Home', user=user, messages=messages)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()  # currently uses flask stuff but ngl looks bad so gonna fix
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/privacy')
 def privacy():
     pass
+    return privacy()
 
 @app.route('/blog')
 def blog():
     pass
+    return blog()
 
 @app.route('/nexus-online')
 def nexus_online():
     user = USER
+    return nexus_online()
 
 @app.route('/contact-us')
 def contact_us():
     pass
+    return contact_us()
