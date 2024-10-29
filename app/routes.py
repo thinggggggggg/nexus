748da1321cb1d96a81619616df9296d0693cccdc
thinggggggggg <thomasgotch0413@hotmail.com>
2024-10-29 17:32:01 +0000

    nothing tbh

--- app/routes.py
+++ app/routes.py
@@ -23,7 +23,7 @@ def home():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()  # currently uses flask stuff but ngl looks bad so gonna fix
+    form = LoginForm()
     if form.validate_on_submit():
         flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
         return redirect(url_for('index'))
