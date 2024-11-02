8651a566425c73e6fdc00acc931d04256b6e2f6b
thinggggggggg <thomasgotch0413@hotmail.com>
2024-11-02 21:21:20 +0000

    big css changes

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,8 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username = StringField(validators=[DataRequired()], id="form_item")
-    password = PasswordField(validators=[DataRequired()], id="form_item")
-    remember_me = BooleanField('Remember Me', id="boolean_form_item")
-    submit = SubmitField('Sign In', id="form_item")
+    username = StringField(validators=[DataRequired()], id="username-entry")
+    password = PasswordField(validators=[DataRequired()], id="password-entry")
+    remember_me = BooleanField('Remember Me', id="boolean-form-item")
+    next = SubmitField('Next', id="next-entry")
+    submit = SubmitField('Sign In', id="submit-entry")
