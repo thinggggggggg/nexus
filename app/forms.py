44db1afc24153fa479df65b4d6faf1938be41d90
thinggggggggg <thomasgotch0413@hotmail.com>
2024-11-02 18:09:09 +0000

    more css stuff

--- app/forms.py
+++ app/forms.py
@@ -2,8 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
-class LoginForm(FlaskForm):  # might change... css is better. looks nicer
-    username = StringField(validators=[DataRequired()], id="input_field")
-    password = PasswordField(validators=[DataRequired()], id="input_field")
-    remember_me = BooleanField('Remember Me', id="boolean_field")
-    submit = SubmitField('Sign In', id="submit_field")
+class LoginForm(FlaskForm):
+    username = StringField(validators=[DataRequired()], id="form_item")
+    password = PasswordField(validators=[DataRequired()], id="form_item")
+    remember_me = BooleanField('Remember Me', id="boolean_form_item")
+    submit = SubmitField('Sign In', id="form_item")
